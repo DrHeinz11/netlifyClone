2a4d2c68582592490394f2cca9c0c7a8db13a2ab
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-08-18 20:20:14 -0300

    V1.5.2(text size fixed)

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,10 +1,10 @@
 const Header = () => {
   return (
     <div className="flex flex-col gap-4 aling-center px-4 mb-4">
-      <h1 className="font-display font-semibold text-4xl text-center">
+      <h1 className="font-display font-semibold text-3xl text-center">
         Bring it all together
       </h1>
-      <p className="font-body text-lg font-medium  text-center">
+      <p className="font-body text-xl font-medium  text-center">
         The fastest way to combine your favorite tools and APIs to build the
         fastest sites, stores, and apps for the web.
       </p>
